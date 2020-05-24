5fa158e72db870984816c4ef35a5e4a540f109f2
sancaryum <sancaryum@gmail.com>
2020-05-25 02:13:43 +0300

    deneme.txt added

--- app/src/main/java/com/example/files/MainActivity.java
+++ app/src/main/java/com/example/files/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.files;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,20 +45,44 @@ public class MainActivity extends AppCompatActivity {
             //BufferedReader ile satır satır okuyabiliriz (BORU-3)
             BufferedReader br = new BufferedReader(isr);
 
-            TextView tv = findViewById(R.id.textView);
+            tv = findViewById(R.id.textView);
             tv.setText(br.readLine());
 
             //Yazdıktan sonra fileInputStream objesinin yetkisini geri kapatıyoruz.
             fis.close();
-
-
-
-
-
-
         }catch (Exception e){
             e.printStackTrace();
         }
+
+        try{
+            //Uygulamanın kaynaklarına erişebilmek için pointer tanımlıyoruz.
+            Resources r = getResources();
+            //Girdi şeklinde bir akış oluşturuyoruz
+            InputStream is = r.openRawResource(R.raw.dene);
+            Scanner s = new Scanner(is);
+            String str="";
+            //yazılı bir şeyler oldukça, tüm yazılanları bir stringe ata
+            while (s.hasNext()){
+                str+= " " + s.next();
+            }
+            tv.setText(str);
+
+            /*
+            bu parça sadece 1 satır 1 satır okutur.
+            tv.setText(s.nextLine());
+             */
+
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
 
     }
 }
